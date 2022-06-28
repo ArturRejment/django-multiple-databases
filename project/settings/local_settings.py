010f4566be45f4eb73e96a585a6698897e329b90
Artur Rejment <arturrejment1@gmail.com>
2022-06-28 23:06:39 +0200

    feat: add SQL queries logger for test environment

--- project/settings/local_settings.py
+++ project/settings/local_settings.py
@@ -1,3 +1,25 @@
 from project.settings.base_settings import *
 
 DEBUG = True
+
+LOGGING = {
+    'version': 1,
+    'filters': {
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue',
+        }
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+        }
+    },
+    'loggers': {
+        'django.db.backends': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+        }
+    }
+}
